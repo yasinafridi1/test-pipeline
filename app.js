b49b30dde66f40ddb9e361a35bd3f3da9417a633
yasinafridi1 <yaseenafridi10875@gmail.com>
2024-12-16 00:46:11 +0500

    added yml

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.set("Content-Type", "text/html; charset=utf-8");
-  res.send("<h1>Welcome to the app</h1>");
+  res.send("<h1>Auto deployed doone</h1>");
 });
 
 app.listen(process.env.PORT);
